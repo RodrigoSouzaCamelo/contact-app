a60f7a643243a30f440503efdc2005ecc1bb8e49
Rodrigo Souza Camelo <rodrigo.souza.camelo@gmail.com>
2023-02-10 16:53:36 -0300

    Create function getById and delete

--- app/src/main/java/br/com/rodrigo/contactapp/database/ContactDAO.kt
+++ app/src/main/java/br/com/rodrigo/contactapp/database/ContactDAO.kt
@@ -2,14 +2,21 @@ package br.com.rodrigo.contactapp.database
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy.REPLACE
 import androidx.room.Query
 import br.com.rodrigo.contactapp.models.Contact
 
 @Dao
 interface ContactDAO {
-    @Insert
+    @Insert(onConflict = REPLACE)
     fun insert(contact: Contact)
 
     @Query("SELECT * FROM Contact")
     fun getAll(): List<Contact>
+
+    @Query("SELECT * FROM Contact WHERE id = :id")
+    fun getById(id: Long): Contact?
+
+    @Query("DELETE FROM Contact WHERE id = :id")
+    fun delete(id: Long)
 }
