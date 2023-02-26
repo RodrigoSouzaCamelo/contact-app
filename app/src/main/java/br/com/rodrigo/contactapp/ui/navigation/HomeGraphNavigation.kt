9c38e53a0708f4ab8c73182a5f0b5275b2a67d17
Rodrigo Souza Camelo <rodrigo.souza.camelo@gmail.com>
2023-02-25 23:59:01 -0300

    Setup routes

--- app/src/main/java/br/com/rodrigo/contactapp/ui/navigation/HomeGraphNavigation.kt
+++ app/src/main/java/br/com/rodrigo/contactapp/ui/navigation/HomeGraphNavigation.kt
@@ -2,12 +2,14 @@ package br.com.rodrigo.contactapp.ui.navigation
 
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.composable
 import br.com.rodrigo.contactapp.ui.screens.home.HomeScreen
 import br.com.rodrigo.contactapp.ui.screens.home.HomeViewModel
+import kotlinx.coroutines.launch
 
 fun NavGraphBuilder.homeGraph(
     navController: NavHostController
@@ -16,8 +18,15 @@ fun NavGraphBuilder.homeGraph(
         val viewModel = hiltViewModel<HomeViewModel>()
         val state by viewModel.uiState.collectAsState()
 
+        val coroutineScope = rememberCoroutineScope()
         HomeScreen(
             state = state,
+            onClickSignOut = {
+                coroutineScope.launch {
+                    viewModel.signOut(  )
+                    navController.navegaParaLoginDeslogado()
+                }
+            },
             onClickOpenDetails = {
                 navController.navigateToContactDetails(it)
             },
--- app/src/main/java/br/com/rodrigo/contactapp/ui/screens/registration/RegistrationViewModel.kt
+++ app/src/main/java/br/com/rodrigo/contactapp/ui/screens/registration/RegistrationViewModel.kt
@@ -1,18 +1,27 @@
 package br.com.rodrigo.contactapp.ui.screens.registration
 
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
 import androidx.lifecycle.ViewModel
+import br.com.rodrigo.contactapp.preferences.PreferencesKey
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+import javax.inject.Inject
 
-class RegistrationViewModel : ViewModel() {
+@HiltViewModel
+class RegistrationViewModel @Inject constructor(
+    private val dataStore: DataStore<Preferences>
+) : ViewModel() {
     private val _uiState = MutableStateFlow(RegistrationUiState())
     val uiState: StateFlow<RegistrationUiState>
-        get() = _uiState.asStateFlow()
+    get() = _uiState.asStateFlow()
 
     init {
-        _uiState.update {state ->
+        _uiState.update { state ->
             state.copy(
                 onChangeName = {
                     _uiState.value = _uiState.value.copy(
@@ -30,6 +39,14 @@ class RegistrationViewModel : ViewModel() {
                     )
                 }
             )
+        }
+    }
+    suspend fun saveLogin() {
+        dataStore.edit { preferences ->
+            preferences[PreferencesKey.USER] =
+                _uiState.value.user
+            preferences[PreferencesKey.PASSWORD] =
+                _uiState.value.password
         }
     }
 }
